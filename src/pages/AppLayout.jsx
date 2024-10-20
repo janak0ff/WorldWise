651168ac808bdd07d70aa9f737b75bcf26bb2f2f
Janak kr shrestha <janakkrshrestha@gmail.com>
2024-10-20 20:32:59 +0545

    Building the App Layout

--- src/pages/AppLayout.jsx
+++ src/pages/AppLayout.jsx
@@ -1,12 +1,12 @@
-import AppNav from "../components/AppNav";
-import PageNav from "../components/PageNav";
+import Map from "../components/Map";
+import Sidebar from "../components/Sidebar";
+import styles from "./AppLayout.module.css";
 
 function AppLayout() {
   return (
-    <div>
-      <PageNav />
-      <p>App</p>
-      <AppNav />
+    <div className={styles.app}>
+      <Sidebar />
+      <Map />
     </div>
   );
 }
