9797f4206878f15ab1399710c30d890eb0ebd108
Janak kr shrestha <janakkrshrestha@gmail.com>
2024-10-22 11:39:45 +0545

    Programmatic Navigation with useNavigate

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import AppLayout from "./pages/AppLayout";
 import PageNotFound from "./pages/PageNotFound";
 import Login from "./pages/Login";
 import CityList from "./components/CityList";
+import Form from "./components/Form";
 import City from "./components/City";
 import CountryList from "./components/CountriyList";
 
@@ -54,7 +55,7 @@ function App() {
               path="countries"
               element={<CountryList cities={cities} isLoading={isLoading} />}
             />
-            <Route path="form" element={<p>form</p>} />
+            <Route path="form" element={<Form />} />
           </Route>
           <Route path="*" element={<PageNotFound />} />
         </Routes>
